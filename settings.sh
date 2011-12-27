dcc6569a9ceb76725f4d4637ae4764646a9922de
Callum Jones <callum@callumj.com>
2011-12-27 13:20:25 +0800

    File for setting up aliases

---
+++ settings.sh
@@ -0,0 +1,16 @@
+alias mdbgo="sh ~/.user_data/startmongo.sh"
+alias memgo="memcached -vvvvv"
+alias passgo="passenger start"
+alias cpyg="git url | tr -d '\n' | pbcopy"
+alias reload="source ~/.bash_profile"
+alias ls="ls -l"
+alias testgo="bundle exec spork cucumber"
+alias testall="cucumber features"
+alias masterpull="git checkout master && git pull; git checkout development"
+alias foundation="consular start foundation"
+alias autonetdisable="sudo launchctl unload -w /System/Library/LaunchDaemons/com.callumj.autoswitch.network.plist"
+alias autonetenable="sudo launchctl load -w /System/Library/LaunchDaemons/com.callumj.autoswitch.network.plist"
+
+CUSTOM_PATH=$HOME/bin:/usr/local/sbin
+export EDITOR=mate
+export TERM_EDITOR=vi
